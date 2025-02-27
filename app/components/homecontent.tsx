aeb94ad2b620b35f4ee18b11059e64790925a7e8
Matias Tervonen <matias.tervonen@hotmail.com>
2025-02-27 20:41:44 +0200

    fix

--- app/components/homecontent.tsx
+++ app/components/homecontent.tsx
@@ -24,62 +24,59 @@ export default function HomeContent() {
   return (
     <>
       <div className=" flex flex-col-reverse  justify-center md:mt-20 md:flex-row">
-        <div className="bg-blue-200  md:mr-5 md:rounded-xl md:pr-20">
-          <div className="p-5 text-xl text-gray-600 hidden md:block">
-            <DateTimeDisplay />
+        <div className="bg-blue-200 flex flex-col md:mr-5 md:rounded-xl md:pr-20 justify-between">
+          <div>
+            <div className="p-5 text-xl text-gray-600 hidden md:block">
+              <DateTimeDisplay />
+            </div>
+            <div className="flex flex-wrap justify-center  md:flex-col ">
+              <div className="p-5 text-xl font-bold text-nowrap ">
+                <button
+                  onClick={() => setActiveMap("today")}
+                  className={`p-2 ${
+                    activeMap === "today"
+                      ? "text-blue-700 underline"
+                      : "text-gray-600"
+                  }`}
+                >
+                  Weather Now
+                </button>
+              </div>
+              <div className="p-5 text-xl font-bold text-nowrap">
+                <button
+                  onClick={() => setActiveMap("tomorrow")}
+                  className={`p-2 ${
+                    activeMap === "tomorrow"
+                      ? "text-blue-700 underline"
+                      : "text-gray-600"
+                  }`}
+                >
+                  Tomorrow
+                </button>
+              </div>
+              <div className="p-5 text-xl font-bold text-nowrap">
+                <button
+                  onClick={() => setActiveMap("dayaftertomorrow")}
+                  className={`p-2 ${
+                    activeMap === "dayaftertomorrow"
+                      ? "text-blue-700 underline"
+                      : "text-gray-600"
+                  }`}
+                >
+                  Day after tomorrow
+                </button>
+              </div>
+            </div>
           </div>
-          <div className="flex flex-wrap justify-center  md:flex-col">
-            <div className="p-5 text-xl font-bold text-nowrap ">
-              <button
-                onClick={() => setActiveMap("today")}
-                className={`p-2 ${
-                  activeMap === "today"
-                    ? "text-blue-700 underline"
-                    : "text-gray-600"
-                }`}
-              >
-                Weather Now
-              </button>
-            </div>
-            <div className="p-5 text-xl font-bold text-nowrap">
-              <button
-                onClick={() => setActiveMap("tomorrow")}
-                className={`p-2 ${
-                  activeMap === "tomorrow"
-                    ? "text-blue-700 underline"
-                    : "text-gray-600"
-                }`}
-              >
-                Tomorrow
-              </button>
-            </div>
-            <div className="p-5 text-xl font-bold text-nowrap">
-              <button
-                onClick={() => setActiveMap("dayaftertomorrow")}
-                className={`p-2 ${
-                  activeMap === "dayaftertomorrow"
-                    ? "text-blue-700 underline"
-                    : "text-gray-600"
-                }`}
-              >
-                Day after tomorrow
-              </button>
-            </div>
-          </div>
+          <div className="p-4 text-gray-600 text-center">Data Finnish Meteorological Institute</div>
         </div>
 
         {/* ✅ FIX: Set fixed height for the weather map container */}
-        <div className="flex justify-center  md:pt-0">
+        <div className="flex justify-center md:pt-0">
           <div className=" md:bg-blue-300 rounded-xl p-10 pt-5 md:p-20">
             {renderMap}
           </div>
         </div>
-      </div>
-
-      {/* Footer */}
-
-      <div className="fixed  p-2 pl-4 text-white  font-bold bottom-0 left-0 w-full ">
-        Powered by weather data from the Finnish Meteorological Institute
       </div>
     </>
   );
