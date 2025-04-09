c3a14ed7cd18c6add537c1949a303630912d4435
Matias Tervonen <matias.tervonen@hotmail.com>
2025-04-09 23:03:33 +0300

    add call to the site after revalidation to avoid cold starts

--- app/api/revalidate-homepage/route.ts
+++ app/api/revalidate-homepage/route.ts
@@ -1,24 +1,34 @@
 import { revalidatePath } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 export async function GET(request: NextRequest) {
-    const authHeader = request.headers.get("authorization");
-    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-        return new Response("Unauthorized", {
-        status: 401,
-        });
-    }
-    
-    try {
-        revalidatePath("/");
-    
-        return NextResponse.json({ success: true });
-    } catch (error) {
-        console.error("Error revalidating:", error);
-        return NextResponse.json(
-        { message: "Error revalidating", error },
-        { status: 500 }
-        );
-    }
+  const authHeader = request.headers.get("authorization");
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response("Unauthorized", {
+      status: 401,
+    });
+  }
+
+  try {
+    revalidatePath("/");
+
+    // Optional: small delay to ensure invalidation has propagated
+    await sleep(100);
+
+    await fetch(`${process.env.BASE_URL}`, {
+      headers: {
+        "User-Agent": "CronBot",
+      },
+    });
+
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error("Error revalidating:", error);
+    return NextResponse.json(
+      { message: "Error revalidating", error },
+      { status: 500 }
+    );
+  }
 }
--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -1,6 +1,8 @@
 import { revalidateTag } from "next/cache";
 import { NextRequest, NextResponse } from "next/server";
 
+const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
+
 export async function GET(request: NextRequest) {
   const authHeader = request.headers.get("authorization");
   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
@@ -11,7 +13,16 @@ export async function GET(request: NextRequest) {
 
   try {
     revalidateTag("weather-map");
-  
+
+    // Optional: small delay to ensure invalidation has propagated
+    await sleep(100);
+
+    // Force a fetch to trigger the rebuild
+    await fetch(`${process.env.BASE_URL}`, {
+      headers: {
+        "User-Agent": "CronBot",
+      },
+    });
 
     return NextResponse.json({ success: true });
   } catch (error) {
