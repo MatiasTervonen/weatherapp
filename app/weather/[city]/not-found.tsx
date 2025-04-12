e62446bf76126d8a33a672638a64b053cbc3056c
Matias Tervonen <matias.tervonen@hotmail.com>
2025-04-12 10:58:33 +0300

    add fallback if searched city could not be found

---
+++ app/weather/[city]/not-found.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import Link from "next/link";
+import { useTranslation } from "@/app/lib/useTranslation";
+
+export default function NotFound() {
+  const { t } = useTranslation("NotFound");
+
+  return (
+    <main className="flex justify-center mt-40">
+      <div className="flex flex-col text-center gap-4 md:gap-8 p-5 md:p-10 rounded-xl bg-blue-400 dark:bg-slate-950">
+        <h2 className="text-md md:text-xl lg:text-2xl">{t("404")}</h2>
+        <p className="text-md md:text-xl lg:text-2xl">{t("NotFoundText")}</p>
+        <Link
+          href={"/"}
+          className="rounded-md bg-blue-600 px-4 py-2 text-gray-100 hover:bg-blue-500 hover:scale-95 text-md md:text-xl lg:text-2xl border-2 border-gray-100"
+        >
+          {t("GoBack")}
+        </Link>
+      </div>
+    </main>
+  );
+}
