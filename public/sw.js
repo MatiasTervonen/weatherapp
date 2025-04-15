cec8e02b8db8e81f0618bc39bf3b4e4d5cc1832a
Matias Tervonen <matias.tervonen@hotmail.com>
2025-04-15 19:22:19 +0300

    Update sw.j

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,44 @@
-self.addEventListener("install", () => {
+const CACHE_NAME = "weather-app-cache-v1";
+const urlsToCache = [
+  "/site.webmanifest",
+  "/favicon-32x32.png",
+  "/favicon-16x16.png",
+  "/android-chrome-192x192.png",
+  "/android-chrome-512x512.png",
+];
+
+self.addEventListener("install", (event) => {
   console.log("Service Worker: Installed");
   self.skipWaiting(); // Forces the service worker to activate immediately
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate", (event) => {
   console.log("Service Worker: Activated");
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            console.log("Service Worker: Clearing Old Cache");
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
-  // Basic fetch handler - responds with the network request
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
